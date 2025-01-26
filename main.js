3e19edfb74ebb1ec67c9d2f40a361ca6554f1124
Cee Jay <52139555+Somto811@users.noreply.github.com>
2025-01-26 13:20:34 -0900

    Update main.js

--- main.js
+++ main.js
@@ -115,7 +115,7 @@ const newAgent = (proxy = null) => {
 
 // LayerEdge Connection Class
 class LayerEdgeConnection {
-    constructor(proxy = null, privateKey = null, refCode = "knYyWnsE") {
+    constructor(proxy = null, privateKey = null, refCode = "A1YWNyu7") {
         this.refCode = refCode;
         this.proxy = proxy;
 
